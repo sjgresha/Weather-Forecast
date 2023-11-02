c6c4fe7117978722b54d1f77a6723189740e1247
Steven Gresham <sbjg2545@gmail.com>
2023-11-02 16:59:44 -0700

    worked on html and js and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 var apiKey = '6c03a1931a75b88a1f2deda71ffad700'
 
-//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} Dont use
+//different api links, not sure which would work
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 ///https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 
-//needs to be in function
-// var urlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+//need to add forecast function
 //var urlWeatherForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
 var citySearchBar = document.querySelector('#city-search-bar');
@@ -12,15 +12,15 @@ var searchBtn = document.querySelector('#search-btn');
 var weatherDisplay = document.querySelector('#weather-display')
 var forecastDisplay = document.querySelector('#forecast-display')
 
-// searchBtn.addEventListener('click', function(event) {
-//     searchCity(event);
-// });
+searchBtn.addEventListener('click', function(event) {
+    searchCity(event);
+});
 
-// function searchCity(event) {
-//     event.preventDefault();
-//     var city = citySearchBar.value.trim();
-
-// }
+function searchCity(event) {
+    event.preventDefault();
+    var city = citySearchBar.value.trim();
+    weather(city);
+}
 
 function weather(city) {
     
@@ -32,8 +32,11 @@ function weather(city) {
         })
         .then(function (data) {
             console.log("current weather: ", data)
+            document.getElementById('city-name').innerText = data.name;
+            document.getElementById('city-temp').innerText = data.main.temp;
+            document.getElementById('city-humidity').innerText = data.main.humidity;
+            document.getElementById('city-wind').innerText = data.wind.speed;
         })
         .catch((err) => console.log(err));
 };
 
-weather();
