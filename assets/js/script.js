b5a3c8ab73543159a4128974dc68ab8ee6be8d84
Steven Gresham <sbjg2545@gmail.com>
2023-11-02 15:40:05 -0700

    initial code

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var apiKey = '6c03a1931a75b88a1f2deda71ffad700'
+
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} Dont use
+///https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
